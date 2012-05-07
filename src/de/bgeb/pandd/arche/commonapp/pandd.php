495a9cca9eb3365b188ff873d91bbdb76464a5d8
Bastian Gebhardt <info@bastian-gebhardt.de>
2012-05-07 21:51:19 +0200

    constant PANDD_ENVIRONMEND implemented; include path extension removed; underscores in namespaces now allowed

--- src/de/bgeb/pandd/arche/commonapp/pandd.php
+++ src/de/bgeb/pandd/arche/commonapp/pandd.php
@@ -3,53 +3,46 @@
 class Pandd{
 	
 	public static function bootStrap(){
-		
-		self::setRootConsts();
-		
-		self::setIncludePath();
-		
-		self::addAutoLoader();
-		
-	}
-	
-	public static function testBootstrap(){
-	
-		self::setRootConsts();
-	
-		self::setIncludePath();
-	
-		self::addAutoLoader();
-	
-	}
 
+		define('PANDD_ENVIRONMENT','@add_environment_here@');
+		
+		self::setRootConsts();
+		
+		self::addAutoLoader();
+		
+	}
+	
 	private static function setRootConsts(){
 		define('PANDD_APP_ROOT',dirname(__FILE__));
 		define('PANDD_SRC_ROOT',PANDD_APP_ROOT.'/src');
 		define('PANDD_TEST_ROOT',PANDD_APP_ROOT.'/test');
-		define('PANDD_CLASS_ROOT',PANDD_APP_ROOT.'/lib');
+		define('PANDD_LIB_ROOT',PANDD_APP_ROOT.'/lib');
 	}
 	
-	private static function setIncludePath(){
-		set_include_path(
-				PANDD_CLASS_ROOT.PATH_SEPARATOR
-				.PANDD_SRC_ROOT.PATH_SEPARATOR
-				.PANDD_TEST_ROOT.PATH_SEPARATOR
-				.get_include_path()
-		);
-	}
-		
 	private static function addAutoLoader(){
 		spl_autoload_register(array(__CLASS__, 'userAutoLoader'));
 	}
 	private static function userAutoLoader($className){
-		$classfile = str_replace('\\', '/', ltrim($className, '\\'));
-		$classfile = str_replace('_', '/', ltrim($classfile, '_')) . '.php';
-		$srcfile = PANDD_SRC_ROOT.'/'.$classfile;
-		$libfile = PANDD_CLASS_ROOT.'/'.$classfile;
-		$testfile = PANDD_TEST_ROOT.'/'.$classfile;
-		if(	is_file($srcfile) || is_file($libfile) || is_file($testfile)){
-			require_once $classfile;
+		if (strpos($className, "\\")===false){
+			$classfile = str_replace('_', '/', ltrim($className, '_')) . '.php';
 		}
+		else {
+			$classfile = str_replace('\\', '/', ltrim($className, '\\')) . '.php';
+		}
+		
+		$srcfile = PANDD_SRC_ROOT.'/'.$classfile;
+		$libfile = PANDD_LIB_ROOT.'/'.$classfile;
+		$testfile = PANDD_TEST_ROOT.'/'.$classfile;
+		if(	is_file($srcfile)){
+			require_once $srcfile;
+		}
+		else if (is_file($libfile)){
+			require_once $libfile;
+		}
+		else if (is_file($testfile)){
+			require_once $testfile;
+		}
+
 		// else continue with the next autoloader
 	}
 
